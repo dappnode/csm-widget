02abf5e6674fe185299bbd1676efc8444feb9c6d
Mateu Miralles <52827122+mateumiralles@users.noreply.github.com>
2025-02-04 14:21:22 +0100

    fix: execution gates on loading (#5)

--- shared/navigate/gates/gate-loaded.tsx
+++ shared/navigate/gates/gate-loaded.tsx
@@ -3,11 +3,6 @@ import { FC, PropsWithChildren, ReactNode } from 'react';
 import { useAccount, useCsmPaused, useCsmPublicRelease } from 'shared/hooks';
 import { useCsmEarlyAdoption } from 'shared/hooks/useCsmEarlyAdoption';
 import { SplashPage } from '../splash';
-// DAPPNODE
-import { useECSanityCheck } from 'dappnode/hooks/use-ec-sanity-check';
-import { ECNotInstalledPage } from 'dappnode/fallbacks/ec-not-installed-page';
-import { ECNoLogsPage } from 'dappnode/fallbacks/ec-no-logs-page';
-import { ECSyncingPage } from 'dappnode/fallbacks/ec-syncing-page';
 import { ECScanningPage } from 'dappnode/fallbacks/ec-scanning-events';
 
 type Props = {
@@ -16,7 +11,6 @@ type Props = {
 };
 
 export const GateLoaded: FC<PropsWithChildren<Props>> = ({
-  fallback,
   additional,
   children,
 }) => {
@@ -25,43 +19,16 @@ export const GateLoaded: FC<PropsWithChildren<Props>> = ({
   const { isConnecting } = useAccount();
   const { isListLoading, active } = useNodeOperatorContext();
   const { initialLoading: isEaLoading } = useCsmEarlyAdoption();
+
   // DAPPNODE
-  const {
-    isInstalled,
-    isLoading: isECLoading,
-    isSynced,
-    hasLogs,
-  } = useECSanityCheck();
+  const fallback = isListLoading ? <ECScanningPage /> : <SplashPage />;
 
   const loading =
     isPublicReleaseLoading ||
     isPausedLoading ||
     isConnecting ||
     isListLoading ||
-    (!active && isEaLoading) ||
-    isECLoading; // DAPPNODE
+    (!active && isEaLoading);
 
-  // DAPPNODE
-  isECLoading ? (
-    <SplashPage />
-  ) : !isInstalled ? (
-    <ECNotInstalledPage />
-  ) : !isSynced ? (
-    <ECSyncingPage />
-  ) : !hasLogs ? (
-    <ECNoLogsPage />
-  ) : isListLoading ? (
-    <ECScanningPage />
-  ) : (
-    <SplashPage />
-  );
-
-  // DAPPNODE
-  return (
-    <>
-      {loading || !isInstalled || !isSynced || !hasLogs || additional
-        ? fallback
-        : children}
-    </>
-  );
+  return <>{loading || additional ? fallback : children}</>;
 };
