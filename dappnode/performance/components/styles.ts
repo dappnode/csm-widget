c05c18c37e160496f69a37086075c7f766737501
mateumiralles <mateumiralles714@gmail.com>
2025-12-10 19:27:32 +0100

    fix: rm pendingReports card

--- dappnode/performance/components/styles.ts
+++ dappnode/performance/components/styles.ts
@@ -216,10 +216,12 @@ export const NoteWrapper = styled.div`
 
 export const PerformanceCardStyle = styled(StackStyle).attrs({ $gap: 'sm' })`
   border-radius: ${({ theme }) => theme.borderRadiusesMap.md}px;
-  padding: 16px 22px;
+  padding: 22px;
   background: var(--lido-color-foreground);
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
   position: relative;
 `;
 
--- dappnode/performance/performance-cards-section.tsx
+++ dappnode/performance/performance-cards-section.tsx
@@ -1,14 +1,10 @@
 import { Stack } from 'shared/components';
 import { PerformanceCard } from './components/performance-card';
-import { Link, Loader } from '@lidofinance/lido-ui';
-import { dappnodeLidoDocsUrls } from 'dappnode/utils/dappnode-docs-urls';
-import { useGetPendingReports } from 'dappnode/hooks/use-get-pending-reports';
+('dappnode/utils/dappnode-docs-urls');
 import { useFrameInfo } from 'modules/web3';
 import { countCalendarDaysLeft } from 'utils';
 
 export const PerformanceCardsSection = () => {
-  const { pendingReports, isLoading } = useGetPendingReports();
-
   const { data: rewardsFrame } = useFrameInfo((data) => ({
     nextDistribution: data.lastReport + data.frameDuration,
   }));
@@ -23,28 +19,6 @@ export const PerformanceCardsSection = () => {
         tooltip="The time remaining until the next Lido CSM report"
       >
         {daysUntilNextReport} {daysUntilNextReport === 1 ? 'day' : 'days'}
-      </PerformanceCard>
-      <PerformanceCard
-        title="Pending data:"
-        tooltip={
-          <p>
-            This represents the number of reports yet to be processed. If you
-            have active validators, it may include the performance of them.
-            These reports will be parsed automatically within the next hours.
-            Learn more about it in our{' '}
-            <Link href={dappnodeLidoDocsUrls.pendingHashes}>
-              our Documentation
-            </Link>
-          </p>
-        }
-      >
-        {isLoading ? (
-          <Loader size="small" />
-        ) : (
-          <p>
-            {pendingReports} {pendingReports === 1 ? 'report' : 'reports'}
-          </p>
-        )}
       </PerformanceCard>
     </Stack>
   );
