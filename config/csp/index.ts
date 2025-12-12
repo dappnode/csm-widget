61046fc39c5be257e007554aba8b4119434ee33b
mateumiralles <mateumiralles714@gmail.com>
2025-12-12 11:50:32 +0100

    fix: force `unsafe-eval` in prod

--- config/csp/index.ts
+++ config/csp/index.ts
@@ -29,7 +29,7 @@ export const contentSecurityPolicy: ContentSecurityPolicyOption = {
     scriptSrc: [
       "'self'",
       "'unsafe-inline'",
-      ...(config.developmentMode ? ["'unsafe-eval'"] : []), // for HMR
+      "'unsafe-eval'", // for HMR
       ...trustedHosts,
     ],
 
